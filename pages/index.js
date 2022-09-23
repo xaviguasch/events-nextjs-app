fc5fb27af7dac2cb8f15acc514f3e7a4d42d1be4
xaviguasch <xaviguasch@gmail.com>
2022-09-23 16:22:41 +0200

    wired data to components and added styling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import { getFeaturedEvents } from '../dummy-data'
 
+import EventList from '../components/events/event-list'
+
 const HomePage = () => {
-  const getFeaturedEvents = getFeaturedEvents()
+  const featuredEvents = getFeaturedEvents()
 
   return (
     <div>
       <h1>The home page</h1>
+      <EventList items={featuredEvents} />
     </div>
   )
 }
