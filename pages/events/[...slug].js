f7cfa6f905f5c10268fb2fd07c925ad63da3a6a7
xaviguasch <xaviguasch@gmail.com>
2022-09-24 16:33:16 +0200

    extracted data on the slug page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+
+import { getFilteredEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
 
 const FilteredEventsPage = () => {
+  const router = useRouter()
+
+  const filterData = router.query.slug
+
+  // The first time the component renders, filterData will be empty due to
+  // how React extracts routes, that's why we need the below statement.
+  if (!filterData) {
+    return <p className='center'>Loading...</p>
+  }
+
+  const filteredYear = filterData[0]
+  const filteredMonth = filterData[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
   return (
     <div>
       <h1>Filtered Events</h1>
+      <EventList items={filteredEvents} />
     </div>
   )
 }
