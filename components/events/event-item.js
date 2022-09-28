0313f85f342517edb435c7176482a0fa5179cafd
xaviguasch <xaviguasch@gmail.com>
2022-09-28 20:48:53 +0200

    adjusted bug with the previous image optimization

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -21,7 +21,7 @@ const EventItem = (props) => {
 
   return (
     <li className={classes.item}>
-      <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
