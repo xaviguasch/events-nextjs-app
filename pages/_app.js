2d574eaf4ec30915599c65864bfd49e2dda59e73
xaviguasch <xaviguasch@gmail.com>
2022-09-28 12:07:05 +0200

    added general Head via _app.js component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
 import Layout from '../components/layout/layout'
@@ -5,6 +7,11 @@ import Layout from '../components/layout/layout'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
